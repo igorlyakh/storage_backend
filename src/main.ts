62c48e3fb06b19cc11fe42d91730c89337cf7bd8
Igor Liakh <igorlyah060@gmail.com>
2026-01-23 18:29:42 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 3001;
+
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'all',
+  });
+
+  await app.listen(PORT, () => console.log('Сервер запущен.'));
+}
+bootstrap();
