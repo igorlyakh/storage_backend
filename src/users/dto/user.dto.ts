94eccaf2b47cb73ed599b6e8947bdef247bb9910
Igor Liakh <igorlyah060@gmail.com>
2026-01-30 13:52:34 +0200

    added create user, find user

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,4 +1,16 @@
+import { IsEnum, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Role } from 'generated/prisma/enums';
+
 export class UserDto {
+  @IsNotEmpty()
+  @IsString()
   username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6, { message: 'Password is required!' })
   password: string;
+
+  @IsEnum(Role)
+  role: Role;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
@@ -8,6 +9,21 @@ export class UsersService {
   async findByUsername(username: string) {
     const user = await this.prisma.user.findUnique({
       where: { username },
+    });
+    return user;
+  }
+
+  async findUserById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+    });
+    return user;
+  }
+
+  async createUser(userDto: UserDto) {
+    const user = this.prisma.user.create({
+      data: { ...userDto },
+      include: { store: true, warehouseRequests: true },
     });
     return user;
   }
