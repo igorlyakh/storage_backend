30575b49a7aa2537f396622182e2e70951ceaa27
Igor Liakh <igorlyah060@gmail.com>
2026-02-01 20:47:24 +0200

    added stores servire with funcs create, find, delete

---
+++ src/stores/stores.service.ts
@@ -0,0 +1,30 @@
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateStoreDto } from './dto/createStore.dto';
+
+@Injectable()
+export class StoresService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findStoreByName(name: string) {
+    const store = await this.prisma.store.findUnique({ where: { name } });
+    return store;
+  }
+
+  async createStore(dto: CreateStoreDto) {
+    const candidate = await this.findStoreByName(dto.name);
+    if (candidate) {
+      throw new ConflictException('A store with this name already exists!');
+    }
+    const store = await this.prisma.store.create({ data: { ...dto } });
+    return store;
+  }
+
+  async deleteStoreByName(name: string) {
+    const store = await this.findStoreByName(name);
+    if (!store) {
+      throw new NotFoundException(`Store ${name} not found!`);
+    }
+    return await this.prisma.store.delete({ where: { name: store.name } });
+  }
+}
