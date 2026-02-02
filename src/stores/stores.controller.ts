4a95ace9e58e09239398637ad37d81259ca1f823
Igor Liakh <igorlyah060@gmail.com>
2026-02-02 16:41:31 +0200

    added store res with create and delete store

--- src/stores/stores.controller.ts
+++ src/stores/stores.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Role } from 'generated/prisma/enums';
+import { Roles } from 'src/decorators/role.decorator';
+import { CreateStoreDto } from './dto/createStore.dto';
 import { StoresService } from './stores.service';
 
+@UseGuards(AuthGuard('jwt'))
+@Roles(Role.ADMIN)
 @Controller('stores')
 export class StoresController {
   constructor(private readonly storesService: StoresService) {}
+
+  @Post('')
+  @UsePipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  )
+  async createStore(@Body() dto: CreateStoreDto) {
+    return await this.storesService.createStore(dto);
+  }
 }
