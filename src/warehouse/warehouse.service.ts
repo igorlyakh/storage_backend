3c011799d4d84e76cce1fdbb89569cbac08b9069
Igor Liakh <igorlyah060@gmail.com>
2026-02-04 20:24:13 +0200

    updated decremenat for items

--- src/warehouse/warehouse.service.ts
+++ src/warehouse/warehouse.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { OperationDto } from './dto/operation.dto';
 
@@ -34,6 +34,10 @@ export class WarehouseService {
 
     if (!candidate) {
       throw new NotFoundException('Product not found!');
+    }
+
+    if (candidate.quantity < dto.quantity) {
+      throw new BadRequestException('Not enough item in stock');
     }
 
     const result = await this.prisma.warehouseStock.update({
