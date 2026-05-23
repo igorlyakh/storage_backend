f24a67e2b464c22064ddaa8d1310ec587ada02a6
Igor Liakh <igorlyah060@gmail.com>
2026-05-23 11:16:55 +0300

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -39,7 +39,7 @@ export class AuthController {
     res.cookie('refreshToken', tokens.refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
